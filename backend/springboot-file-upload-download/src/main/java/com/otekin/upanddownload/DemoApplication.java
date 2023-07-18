20c0b1db644fbc244a9b210c438aea56881ef296
Oğuzhan Tekin <119113636+tekinoguzhan@users.noreply.github.com>
2023-07-18 11:32:03 +0300

    Initial commit

---
+++ backend/springboot-file-upload-download/src/main/java/com/otekin/upanddownload/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.otekin.upanddownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
